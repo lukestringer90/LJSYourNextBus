6fe118735d685a7f86780a963c3f43fdd0923370
Luke Stringer <lukestringer630@gmail.com>
2014-01-08 20:58:08 +0000

    Tests for LJSScraperTests (not all passing currently).

---
+++ LJSTravelSouthYorkshireTests/LJSScraperTests.m
@@ -0,0 +1,70 @@
+//
+//  LJSScraperTests.m
+//  LJSTravelSouthYorkshire
+//
+//  Created by Luke Stringer on 08/01/2014.
+//  Copyright (c) 2014 Luke Stringer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "LJSScraper.h"
+
+@interface LJSScraperTests : XCTestCase {
+    LJSScraper *_sut;
+    NSDictionary *_correctData;
+}
+@end
+
+@implementation LJSScraperTests
+
+- (void)setUp {
+    [super setUp];
+    _sut = [[LJSScraper alloc] init];
+    _correctData = [self loadJSONFileNamed:@"tram"];
+}
+
+- (NSDictionary *)loadJSONFileNamed:(NSString *)fileName {
+    NSString* filepath = [[NSBundle bundleForClass:[self class]] pathForResource:fileName ofType:@"json"];
+    NSData *data = [NSData dataWithContentsOfFile:filepath];
+    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+}
+
+- (NSString *)loadHTMLFileNamed:(NSString *)fileName {
+    NSString *path = [[NSBundle mainBundle] pathForResource:fileName ofType: @"html"];
+    return [NSString stringWithContentsOfFile:path encoding:NSASCIIStringEncoding error:nil];
+}
+
+- (void)testScapesDepatures {
+    
+    NSDictionary *correctDepatures = _correctData[LJSDepaturesKey];
+    NSString *html = [self loadHTMLFileNamed:@"tram"];
+    
+    NSDictionary *scrapedData = [_sut scrapeDepatureDataFromHTML:html];
+    NSDictionary *scrapedDepatures = scrapedData[LJSDepaturesKey];
+    
+    XCTAssertEqualObjects(scrapedDepatures, correctDepatures, @"");
+}
+
+- (void)testScrapesStopCode {
+    
+    NSString *correctStopCode = _correctData[LJSStopCodeKey];
+    NSString *html = [self loadHTMLFileNamed:@"tram"];
+    
+    NSDictionary *scrapedData = [_sut scrapeDepatureDataFromHTML:html];
+    NSString *scrapedStopCode = scrapedData[LJSStopCodeKey];
+    
+    XCTAssertEqualObjects(scrapedStopCode, correctStopCode, @"");
+}
+
+- (void)testScrapesStopName {
+    
+    NSString *correctStopName = _correctData[LJSSopNameKey];
+    NSString *html = [self loadHTMLFileNamed:@"tram"];
+    
+    NSDictionary *scrapedData = [_sut scrapeDepatureDataFromHTML:html];
+    NSString *scrapedStopName = scrapedData[LJSSopNameKey];
+    
+    XCTAssertEqualObjects(scrapedStopName, correctStopName, @"");
+}
+
+@end
