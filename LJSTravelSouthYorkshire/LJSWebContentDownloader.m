7644ce2e975194c23ff223e9560afcde63a4a3d6
Luke Stringer <lukestringer630@gmail.com>
2014-01-11 15:15:09 +0000

    Content downloader no longer has flag for loading from bundle.

--- LJSTravelSouthYorkshire/LJSWebContentDownloader.m
+++ LJSTravelSouthYorkshire/LJSWebContentDownloader.m
@@ -7,18 +7,9 @@
 //
 
 #import "LJSWebContentDownloader.h"
-
-static BOOL const LoadHTMLFromBundle = YES;
-
 @implementation LJSWebContentDownloader
 
-
 - (NSString *)downloadHTMLFromURL:(NSURL *)url error:(NSError **)error {
-    if (LoadHTMLFromBundle) {
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"tram" ofType: @"html"];
-        return [NSString stringWithContentsOfFile:path encoding:NSASCIIStringEncoding error:nil];
-    }
-    
     return [[NSString alloc] initWithContentsOfURL:url
                                           encoding:NSStringEncodingConversionAllowLossy
                                              error:error];
