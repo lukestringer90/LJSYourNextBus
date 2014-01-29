a189756054570c79d98ee40b4ba87b89091265f5
Luke Stringer <lukestringer630@gmail.com>
2014-01-29 20:22:41 +0000

    Fixed copyright comments.

--- Classes/LJSDepature.m
+++ Classes/LJSDepature.m
@@ -1,9 +1,9 @@
 //
 //  LJSDepature.m
-//  Pods
+//  LJSTravelSouthYorkshire
 //
 //  Created by Luke Stringer on 29/01/2014.
-//
+//  Copyright (c) 2014 Luke Stringer. All rights reserved.
 //
 
 #import "LJSDepature.h"
--- Classes/LJSStop.m
+++ Classes/LJSStop.m
@@ -3,7 +3,7 @@
 //  Pods
 //
 //  Created by Luke Stringer on 29/01/2014.
-//
+//  Copyright (c) 2014 Luke Stringer. All rights reserved.
 //
 
 #import "LJSStop.h"
