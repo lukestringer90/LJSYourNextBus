a12bb6d6bb89be145101118a44134e371fabc584
Luke Stringer <lukestringer630@gmail.com>
2014-03-22 21:39:35 +0000

    Formatting.

--- Classes/LJSYourNextBusClient.h
+++ Classes/LJSYourNextBusClient.h
@@ -8,7 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-
 @class LJSStop;
 @interface LJSYourNextBusClient : NSObject
 
--- Project/LJSYourNextBusTests/LJSYourNextBusTests.m
+++ Project/LJSYourNextBusTests/LJSYourNextBusTests.m
@@ -106,151 +106,160 @@
 #pragma mark - LJSStop
 
 - (void)testStopDetails {
-    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		assertThat(stop.NaPTANCode, equalTo(@"37010071"));
-		assertThat(stop.title, equalTo(@"Rotherham Intc"));
-	}];
+    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   assertThat(stop.NaPTANCode, equalTo(@"37010071"));
+										   assertThat(stop.title, equalTo(@"Rotherham Intc"));
+									   }];
 	
 }
 
 - (void)testStopLiveDate {
-	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		NSDate *correctLiveDate = [self todayAtHours:10 minutes:46];
-		assertThatInteger([stop.liveDate timeIntervalSince1970], equalToInteger([correctLiveDate timeIntervalSince1970]));
-	}];
+	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   NSDate *correctLiveDate = [self todayAtHours:10 minutes:46];
+										   assertThatInteger([stop.liveDate timeIntervalSince1970], equalToInteger([correctLiveDate timeIntervalSince1970]));
+									   }];
 }
 
 - (void)testServicesCount {
-	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		NSArray *services = stop.services;
-		assertThat(services, hasCountOf(4));
-	}];
+	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   NSArray *services = stop.services;
+										   assertThat(services, hasCountOf(4));
+									   }];
 }
 
 #pragma mark - LJSService
 
 - (void)testServicesDetails {
-	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		NSArray *services = [self sortedServicesForStop:stop];
-		
-		LJSService *firstService = services[0];
-		assertThat(firstService.title, equalTo(@"217"));
-		assertThat(firstService.depatures, hasCountOf(2));
-		assertThat(firstService.stop, equalTo(stop));
-		
-		LJSService *secondService = services[1];
-		assertThat(secondService.title, equalTo(@"218"));
-		assertThat(secondService.depatures, hasCountOf(3));
-		assertThat(secondService.stop, equalTo(stop));
-		
-		LJSService *thirdService = services[2];
-		assertThat(thirdService.title, equalTo(@"22"));
-		assertThat(thirdService.depatures, hasCountOf(7));
-		assertThat(thirdService.stop, equalTo(stop));
-		
-		LJSService *fourthService = services[3];
-		assertThat(fourthService.title, equalTo(@"22X"));
-		assertThat(fourthService.depatures, hasCountOf(4));
-		assertThat(fourthService.stop, equalTo(stop));
-	}];
+	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   NSArray *services = [self sortedServicesForStop:stop];
+										   
+										   LJSService *firstService = services[0];
+										   assertThat(firstService.title, equalTo(@"217"));
+										   assertThat(firstService.depatures, hasCountOf(2));
+										   assertThat(firstService.stop, equalTo(stop));
+										   
+										   LJSService *secondService = services[1];
+										   assertThat(secondService.title, equalTo(@"218"));
+										   assertThat(secondService.depatures, hasCountOf(3));
+										   assertThat(secondService.stop, equalTo(stop));
+										   
+										   LJSService *thirdService = services[2];
+										   assertThat(thirdService.title, equalTo(@"22"));
+										   assertThat(thirdService.depatures, hasCountOf(7));
+										   assertThat(thirdService.stop, equalTo(stop));
+										   
+										   LJSService *fourthService = services[3];
+										   assertThat(fourthService.title, equalTo(@"22X"));
+										   assertThat(fourthService.depatures, hasCountOf(4));
+										   assertThat(fourthService.stop, equalTo(stop));
+									   }];
 }
 
 - (void)testDepaturesCount {
-	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		NSArray *allDepatures = [self depaturesForStop:stop];
-		assertThat(allDepatures, hasCountOf(16));
-	}];
+	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   NSArray *allDepatures = [self depaturesForStop:stop];
+										   assertThat(allDepatures, hasCountOf(16));
+									   }];
 	
 }
 
 #pragma mark - LJSDepature
 
 - (void)testDepatureDetails {
-	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		NSArray *services = [self sortedServicesForStop:stop];
-		
-		LJSService *firstService = services[0];
-		LJSService *secondService = services[1];
-		NSArray *firstServiceDepatures = [self sortedDepaturesForService:firstService];
-		NSArray *secondServiceDepatures = [self sortedDepaturesForService:secondService];
-		
-		/**
-		 *  217 	Mexborough 	11:11
-		 */
-		LJSDepature *firstDepatureOfFirstService = firstServiceDepatures[0];
-		assertThat(firstDepatureOfFirstService.destination, equalTo(@"Mexborough"));
-		assertThat(firstDepatureOfFirstService.service, equalTo(firstService));
-		assertThatInteger([firstDepatureOfFirstService.expectedDepatureDate timeIntervalSince1970],
-						  equalToInteger([[self todayAtHours:11 minutes:11] timeIntervalSince1970]));
-		assertThatBool(firstDepatureOfFirstService.hasLowFloorAccess, equalToBool(NO));
-		
-		
-		/**
-		 *  217 	Thurnscoe 	11:41
-		 */
-		LJSDepature *secondDepatureOfFirstService = firstServiceDepatures[1];
-		assertThat(secondDepatureOfFirstService.service, equalTo(firstService));
-		assertThat(secondDepatureOfFirstService.destination, equalTo(@"Thurnscoe"));
-		assertThatInteger([secondDepatureOfFirstService.expectedDepatureDate timeIntervalSince1970],
-						  equalToInteger([[self todayAtHours:11 minutes:41] timeIntervalSince1970]));
-		assertThatBool(secondDepatureOfFirstService.hasLowFloorAccess, equalToBool(NO));
-		
-		
-		/**
-		 *  218 	Barnsley 	10:56
-		 */
-		LJSDepature *firstDepatureOfSecondService = secondServiceDepatures[0];
-		assertThat(firstDepatureOfSecondService.destination, equalTo(@"Barnsley"));
-		assertThat(firstDepatureOfSecondService.service, equalTo(secondService));
-		assertThatInteger([firstDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
-						  equalToInteger([[self todayAtHours:10 minutes:56] timeIntervalSince1970]));
-		assertThatBool(firstDepatureOfSecondService.hasLowFloorAccess, equalToBool(NO));
-		
-		
-		/**
-		 *  218 	Barnsley 	40 mins 	LF
-		 */
-		LJSDepature *secondDepatureOfSecondService = secondServiceDepatures[1];
-		assertThat(secondDepatureOfSecondService.destination, equalTo(@"Barnsley"));
-		assertThat(secondDepatureOfSecondService.service, equalTo(secondService));
-		assertThatInteger([secondDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
-						  equalToInteger([[self date:stop.liveDate plusMinutes:40] timeIntervalSince1970]));
-		assertThatBool(secondDepatureOfSecondService.hasLowFloorAccess, equalToBool(YES));
-		
-		
-		/**
-		 *  218 	Barnsley 	70 mins 	LF
-		 */
-		LJSDepature *thirdDepatureOfSecondService = secondServiceDepatures[2];
-		assertThat(thirdDepatureOfSecondService.destination, equalTo(@"Barnsley"));
-		assertThat(thirdDepatureOfSecondService.service, equalTo(secondService));
-		assertThatInteger([thirdDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
-						  equalToInteger([[self date:stop.liveDate plusMinutes:70] timeIntervalSince1970]));
-		assertThatBool(thirdDepatureOfSecondService.hasLowFloorAccess, equalToBool(YES));
-	}];
+	[self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   NSArray *services = [self sortedServicesForStop:stop];
+										   
+										   LJSService *firstService = services[0];
+										   LJSService *secondService = services[1];
+										   NSArray *firstServiceDepatures = [self sortedDepaturesForService:firstService];
+										   NSArray *secondServiceDepatures = [self sortedDepaturesForService:secondService];
+										   
+										   /**
+											*  217 	Mexborough 	11:11
+											*/
+										   LJSDepature *firstDepatureOfFirstService = firstServiceDepatures[0];
+										   assertThat(firstDepatureOfFirstService.destination, equalTo(@"Mexborough"));
+										   assertThat(firstDepatureOfFirstService.service, equalTo(firstService));
+										   assertThatInteger([firstDepatureOfFirstService.expectedDepatureDate timeIntervalSince1970],
+															 equalToInteger([[self todayAtHours:11 minutes:11] timeIntervalSince1970]));
+										   assertThatBool(firstDepatureOfFirstService.hasLowFloorAccess, equalToBool(NO));
+										   
+										   
+										   /**
+											*  217 	Thurnscoe 	11:41
+											*/
+										   LJSDepature *secondDepatureOfFirstService = firstServiceDepatures[1];
+										   assertThat(secondDepatureOfFirstService.service, equalTo(firstService));
+										   assertThat(secondDepatureOfFirstService.destination, equalTo(@"Thurnscoe"));
+										   assertThatInteger([secondDepatureOfFirstService.expectedDepatureDate timeIntervalSince1970],
+															 equalToInteger([[self todayAtHours:11 minutes:41] timeIntervalSince1970]));
+										   assertThatBool(secondDepatureOfFirstService.hasLowFloorAccess, equalToBool(NO));
+										   
+										   
+										   /**
+											*  218 	Barnsley 	10:56
+											*/
+										   LJSDepature *firstDepatureOfSecondService = secondServiceDepatures[0];
+										   assertThat(firstDepatureOfSecondService.destination, equalTo(@"Barnsley"));
+										   assertThat(firstDepatureOfSecondService.service, equalTo(secondService));
+										   assertThatInteger([firstDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
+															 equalToInteger([[self todayAtHours:10 minutes:56] timeIntervalSince1970]));
+										   assertThatBool(firstDepatureOfSecondService.hasLowFloorAccess, equalToBool(NO));
+										   
+										   
+										   /**
+											*  218 	Barnsley 	40 mins 	LF
+											*/
+										   LJSDepature *secondDepatureOfSecondService = secondServiceDepatures[1];
+										   assertThat(secondDepatureOfSecondService.destination, equalTo(@"Barnsley"));
+										   assertThat(secondDepatureOfSecondService.service, equalTo(secondService));
+										   assertThatInteger([secondDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
+															 equalToInteger([[self date:stop.liveDate plusMinutes:40] timeIntervalSince1970]));
+										   assertThatBool(secondDepatureOfSecondService.hasLowFloorAccess, equalToBool(YES));
+										   
+										   
+										   /**
+											*  218 	Barnsley 	70 mins 	LF
+											*/
+										   LJSDepature *thirdDepatureOfSecondService = secondServiceDepatures[2];
+										   assertThat(thirdDepatureOfSecondService.destination, equalTo(@"Barnsley"));
+										   assertThat(thirdDepatureOfSecondService.service, equalTo(secondService));
+										   assertThatInteger([thirdDepatureOfSecondService.expectedDepatureDate timeIntervalSince1970],
+															 equalToInteger([[self date:stop.liveDate plusMinutes:70] timeIntervalSince1970]));
+										   assertThatBool(thirdDepatureOfSecondService.hasLowFloorAccess, equalToBool(YES));
+									   }];
 }
 
 #pragma mark - Later URL
 
 - (void)testLaterURL {
-    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		assertThat(laterURL.absoluteString, equalTo(@"/pip/stop.asp?naptan=37010071&pscode=218&dest=&offset=1&textonly=1"));
-	}];
+    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   assertThat(laterURL.absoluteString, equalTo(@"/pip/stop.asp?naptan=37010071&pscode=218&dest=&offset=1&textonly=1"));
+									   }];
 }
 
 - (void)testEarlierURL {
 	NSString *html = [self loadHTMLFileNamed:@"37010115"];
 	self.yourNextBusClient.htmlDownloader = [self mockHTMLDownloadReturningHTML:html];
-
-    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		assertThat(earlierURL.absoluteString, equalTo(@"/pip/stop.asp?naptan=37010115&pscode=120&dest=&offset=0&textonly=1"));
-	}];
+	
+    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   assertThat(earlierURL.absoluteString, equalTo(@"/pip/stop.asp?naptan=37010115&pscode=120&dest=&offset=0&textonly=1"));
+									   }];
 }
 
 - (void)testNilEarlierURL {
-    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
-		assertThat(earlierURL, equalTo(nil));
-	}];
+    [self.yourNextBusClient liveDataForNaPTANCode:self.NaPTANCode
+									   completion:^(LJSStop *stop, NSURL *laterURL, NSURL *earlierURL, NSError *error) {
+										   assertThat(earlierURL, equalTo(nil));
+									   }];
 }
 
 @end
