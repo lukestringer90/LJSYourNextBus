bd154847b1ed71a9d7f0451e746c9c31e665e7d9
Luke Stringer <lukestringer630@gmail.com>
2014-03-23 20:56:14 +0000

    App code does not run when tests are running.

--- Project/LJSYourNextBus/LJSAppDelegate.m
+++ Project/LJSYourNextBus/LJSAppDelegate.m
@@ -11,7 +11,17 @@
 
 @implementation LJSAppDelegate
 
+// Helper function to see if we are running the production target or the test target
+static BOOL isRunningTests(void) __attribute__((const));
+static BOOL isRunningTests(void) {
+    NSDictionary* environment = [[NSProcessInfo processInfo] environment];
+    NSString* injectBundle = environment[@"XCInjectBundle"];
+    return [[injectBundle pathExtension] isEqualToString:@"octest"] || [[injectBundle pathExtension] isEqualToString:@"xctest"];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	if (isRunningTests()) return YES;
+	
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	
 	LJSNaPTANEntryViewController *viewController = [[LJSNaPTANEntryViewController alloc] init];
