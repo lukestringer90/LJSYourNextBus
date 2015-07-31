817c72a3dcb4fd8742b6cabc978d0eb7c1328bf5
Luke Stringer <lukestringer90@gmail.com>
2015-07-31 20:03:24 +0100

    Add documentation for Live Data Result and amend Stop

--- Classes/LJSLiveDataResult.h
+++ Classes/LJSLiveDataResult.h
@@ -9,9 +9,20 @@
 #import <Foundation/Foundation.h>
 
 @class LJSStop;
+
+/**
+ *  A Live Data Result object represents the successful parsing of HTML by a Your Next Bus client. A client can finish successfully with a Stop (representing the departures for the next hour) and no messages. A client can also finish with no Stop (when there are no departure) but still generate an array of messages (representing meta information such as warnings). Finally a client can finish with both a Stop and an array of messages.
+ */
 @interface LJSLiveDataResult : NSObject
 
+/**
+ *  The Stop representing the successful parsing of live departure data from HTML. Or nil if the HTML did not contain any data.
+ */
 @property (nonatomic, strong, readonly) LJSStop *stop;
+
+/**
+ *  An array of messages representing meta information such as warnings. Or nil if the HTML did not contain any data.
+ */
 @property (nonatomic, copy, readonly) NSArray *messages;
 
 - (instancetype)initWithStop:(LJSStop *)stop messages:(NSArray *)messages;
