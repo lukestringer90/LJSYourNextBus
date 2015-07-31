2443f789289cbd340307e8ed6ad09cc316fd7adf
Luke Stringer <lukestringer90@gmail.com>
2015-07-31 16:01:42 +0100

    Add documentation for base client

--- Classes/LJSYourNextBusClient.h
+++ Classes/LJSYourNextBusClient.h
@@ -30,25 +30,68 @@ typedef NS_ENUM(NSInteger, LJSYourNextBusError) {
 @class LJSYourNextBusClient, LJSStop;
 @protocol LJSYourNextBusClientDelegate <NSObject>
 
+/**
+ *  Delegate callback when the client has successfully obtained live data.
+ *
+ *  @param client   The Your Next Bus client that has obtained data.
+ *  @param stop     A Stop object representing the live data.
+ *  @param messages A list of messages for the stop live data containing meta information about the stop such as warnings or alerts. Or nil if their are no messages.
+ */
 - (void)client:(LJSYourNextBusClient *)client returnedStop:(LJSStop *)stop messages:(NSArray *)messages;
+
+/**
+ *  Delegate callback when the client has failed to obtained live data.
+ *
+ *  @param client     The Your Next Bus client that has failed.
+ *  @param error      An error detailing the reason for the failure.
+ *  @param NaPTANCode The NaPTAN code of the stop for which the original request to get live data was made for.
+ */
 - (void)client:(LJSYourNextBusClient *)client failedWithError:(NSError *)error NaPTANCode:(NSString *)NaPTANCode;
 
 @end
 
 @protocol LJSYourNextBusScrapeDelegate <NSObject>
 
+/**
+ *  Delegate callback when the client has successfully download the HTML for a NaPTAN code and is about to scrape it for live data.
+ *
+ *  @param client     The Your Next Bus client that will scrape.
+ *  @param HTML       The HTML that will be scraped.
+ *  @param NaPTANCode The NaPTAN code of the stop that the HTML has been obtained for.
+ */
 - (void)client:(LJSYourNextBusClient *)client willScrapeHTML:(NSString *)HTML NaPTANCode:(NSString *)NaPTANCode;
 
 @end
 
+/**
+ *  Use a Your Next Bus Client subclass to get live data for particular stop (specified with a NapTAN code) in a particular region.
+ */
 @interface LJSYourNextBusClient : NSObject
 
-@property (nonatomic, assign) BOOL saveDataToDisk;
+/**
+ *  Delegate to get client callbacks.
+ */
 @property (nonatomic, weak) id<LJSYourNextBusClientDelegate> clientDelegate;
+
+/**
+ *  Delegate to get scrape callbacks.
+ */
 @property (nonatomic, weak) id<LJSYourNextBusScrapeDelegate> scrapeDelegate;
 
+/**
+ *  Asynchronously get live data for a stop as sepcified by a NaPTAN code.
+ *
+ *  @param NaPTANCode The NaPTAN code of the stop to get live data for. For more information see `LJSStop`.
+ */
 - (void)getLiveDataForNaPTANCode:(NSString *)NaPTANCode;
 
+/**
+ *  Construct a URL to the web page where the live data is located for a NaPTAN code. Must be overriden in a subclass to construct the correct form of URL for a specific region.
+ *
+ *  @param stopNumber The NaPTAN code of the stop to get live data for.
+ *
+ *  @return A URL to the web page where live data is location for a stop in a specific region.
+ */
 - (NSURL *)urlForNaPTANCode:(NSString *)stopNumber;
 
 @end
